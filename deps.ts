b614c6125b1456cbf8be440191ab37b23b59fd7e
Tiny Apps <tinyapps1@gmail.com>
2024-11-06 17:49:11 +0530

    added -w

--- deps.ts
+++ deps.ts
@@ -2,7 +2,3 @@ export { parseArgs } from "jsr:@std/cli@^1.0.6";
 export { bundle } from "jsr:@deno/emit@^0.46.0";
 export { resolve } from "jsr:@std/path@^1.0.8";
 
-
-// export { parseArgs } from "https://jsr.io/@std/cli/1.0.6/mod.ts";
-// export { bundle } from "https://jsr.io/@deno/emit/0.46.0/mod.ts";
-// export { resolve } from "https://jsr.io/@std/path/1.0.8/mod.ts";
