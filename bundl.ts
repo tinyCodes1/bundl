cc591395857ac806cee067dafc58851b15cc2a42
Tiny Apps <tinyapps1@gmail.com>
2024-11-29 15:56:49 +0530

    added jsr and npm package parsing

--- bundl.ts
+++ bundl.ts
@@ -4,34 +4,213 @@
  *  bundl : simple wrapper script around deno_emit
  */
 
+import ts from "npm:typescript@^5.7.2";
 import { parseArgs } from "jsr:@std/cli@^1.0.6/parse-args";
 import { bundle } from "jsr:@deno/emit@^0.46.0";
+import {dirname, resolve } from "jsr:@std/path@^1.0.8";
+import { UntarStream } from "jsr:@std/tar@^0.1.4/untar-stream";
 
-const bundl = async(url:string):Promise<string>=> {
+const ifexists=(path: string)=> {
     try {
-        const filePath = url;
-        const res = await bundle(filePath);
-        const { code } = res;
-        return code ;
+        Deno.statSync(path);
+        return true;
     } catch (err) {
-        if (err instanceof Error) {
-            console.error(`Error in ${url} : ${err.message}`);
+        if (err instanceof Deno.errors.NotFound) {
+            return false;
+        } else {
+            throw err;
         }
     }
-    return `err`;
 }
 
-const showHelp=()=> {
-    const parts = Deno.mainModule.split(`/`);
-    const scriptName = parts[parts.length -1];
-    out(
-        `Usage: ${scriptName} [Options] <filename>
-        simple wrapper around deno_emit.
-            Options:
-            -h Show this help message.
-            -o Output file path/name.
-            `);
-        return;
+const download = (url: string, outputPath?: string) => {
+    const bufferSize = 2048000000; // Buffer size for storing the text
+    const sharedBuffer = new SharedArrayBuffer(bufferSize);
+    const textBuffer = new Int32Array(sharedBuffer);
+    if (!outputPath) {
+        outputPath = url.split("/").slice(-1)[0];  ;
+    }
+    Atomics.store(textBuffer, 0, 0);
+    const workerCode = `
+    onmessage = async (e) => {
+        const textBuffer = new Int32Array(e.data.sharedBuffer);
+        const outputPath = e.data.outputPath;
+        try {
+            const response = await fetch(e.data.url);
+            const file = await Deno.open(outputPath, { create: true, write: true });
+            await response.body.pipeTo(file.writable);
+            Atomics.store(textBuffer, 0, 1);
+        } catch (error) {
+            Atomics.store(textBuffer, 0, -1);
+        }
+        Atomics.notify(textBuffer, 0);
+    };
+    `;
+    const blob = new Blob([workerCode], { type: 'application/typescript' });
+    const worker = new Worker(URL.createObjectURL(blob), { type: 'module' });
+    worker.postMessage({ sharedBuffer: sharedBuffer, url: url, outputPath: outputPath });
+    Atomics.wait(textBuffer, 0, 0);
+    const resultCode = Atomics.load(textBuffer, 0);
+    worker.terminate();
+    if (resultCode === -1) {
+        throw new Error(`Failed to fetch or the file size exceeds buffer capacity.`);
+    }
+    return resolve(outputPath);
+};
+
+const readOnlineText = (path: string): string => {
+    const bufferSize = 204800000; // Buffer size for storing the text
+    const sharedBuffer = new SharedArrayBuffer(bufferSize);
+    const textBuffer = new Int32Array(sharedBuffer);
+    Atomics.store(textBuffer, 0, 0);
+    const workerCode = `
+    onmessage = async (e) => {
+        const textBuffer = new Int32Array(e.data.sharedBuffer);
+        try {
+            const response = await fetch(e.data.url);
+            const responseText = await response.text();
+            const encodedText = new TextEncoder().encode(responseText);
+            if (encodedText.length > textBuffer.length) {
+                Atomics.store(textBuffer, 0, -1);
+            } else {
+                textBuffer.set(encodedText.slice(0, textBuffer.length), 0);
+            }
+        } catch (error) {
+            Atomics.store(textBuffer, 0, -1);
+        }
+        Atomics.notify(textBuffer, 0);
+    };
+    `;
+    const blob = new Blob([workerCode], { type: 'application/typescript' });
+    const worker = new Worker(URL.createObjectURL(blob), { type: 'module' });
+    worker.postMessage({ sharedBuffer: sharedBuffer, url: path });
+    Atomics.wait(textBuffer, 0, 0);
+    const resultCode = Atomics.load(textBuffer, 0);
+    worker.terminate();
+    if (resultCode === -1) {
+        throw new Error(`Failed to fetch or the file size exceeds buffer capacity.`);
+    }
+    const decodedText = new TextDecoder().decode(textBuffer);
+    return decodedText.replace(/\0/g, ``).replace(/\s*$/g,``).trim(); 
+}
+
+const readText=(path:string, defaultText=``)=>{
+    if ((path.startsWith(`http://`) || path.startsWith(`https://`))) {
+        return readOnlineText(path).trim();
+    }
+let filetext = ``;
+try {
+    filetext = Deno.readTextFileSync(path);
+} catch (error) {
+    if (error instanceof Deno.errors.NotFound) {
+        writeText(path, defaultText);
+        out(`File created: ${path}`);
+    } else {
+        throw error;
+    }
+}
+return filetext;
+}
+
+const writeText=(path:string, text:string, options?: { whatIfExist?:`overwrite`|`skip`|`append`, verbose?:boolean})=>{
+    const { whatIfExist=`overwrite`, verbose=true} = options || {};
+    const dir = dirname(path);
+    try {
+        Deno.mkdirSync(dir, {recursive:true});
+    } catch (error) {
+        if (error instanceof Deno.errors.AlreadyExists) {
+            if (verbose) {
+                out(`directory already exists: ${dir}`);  
+            }
+        } else {
+            throw error;
+        }
+    }
+    try {
+        if ((ifexists(path)) && (whatIfExist === `skip`)) {
+            if (verbose) {
+                out(`File exists: ${path}. skip.`);
+            }
+            return; 
+        }
+        const appendText = (whatIfExist === `append`) ? true : false;
+        Deno.writeTextFileSync(path, text.trim(), { append: appendText });
+    } catch (error) {
+        throw error;
+    }
+}
+
+const removeDir = async(dir:string) => {
+    try {
+        const list = Deno.readDirSync(dir);
+        for await (const file of list) {
+            const filepath = `${dir}/${file.name}`;
+            if (file.isDirectory) {
+                await removeDir(filepath);
+            } else if (file.isSymlink) {
+                Deno.removeSync(filepath);
+            } else if (file.isFile) {
+                Deno.removeSync(filepath);
+            }
+        }
+        Deno.removeSync(dir);
+    } catch (error) {
+        throw error;
+    }
+}
+
+const getFiles=(dirpath:string, recursive=false, searchword=""):string[]=>{
+    const results = [];
+    const files = Deno.readDirSync(dirpath);
+    for (const file of files) {
+        const fullpath = resolve(dirpath , file.name);
+        if ((file.isDirectory) && (recursive)) {
+            results.push(...getFiles(fullpath, true, searchword));
+        } else {
+            const searchregex = new RegExp(searchword, 'gi');
+            if (searchregex.test(file.name)) {
+                results.push(fullpath);
+            }
+        }
+    }
+    return results;
+}
+
+const untgz=async(fileName:string, outDir?:string)=>{
+    const filePath = resolve(fileName);
+    for await (
+        const entry of ( Deno.openSync(filePath))
+    .readable
+    .pipeThrough(new DecompressionStream("gzip"))
+    .pipeThrough(new UntarStream ())
+    ) {
+        let path = resolve(entry.path);
+        if (outDir) {
+            path = resolve(outDir + S + entry.path);
+        }
+        Deno.mkdirSync(dirname(path), { recursive: true });
+        entry.readable?.pipeTo((Deno.createSync(path)).writable);
+    }
+}
+
+const getOsType=()=>{
+    if (typeof Deno?.build?.os === "string") {
+        return Deno.build.os;
+    }
+    if ((navigator as any)?.appVersion?.includes?.("Win")) {
+        return "windows";
+    }
+    return "linux";
+}
+
+const S = (getOsType() === "windows")? "\\" : "/";
+
+const err=(errText : string, newline:boolean=true)=>{
+    if (newline) {
+        errText += `\n`;
+    }
+    const output = new TextEncoder().encode(errText);
+    Deno.stderr.writeSync(output)
 }
 
 const out=(outputText : string, newline:boolean=true)=>{
@@ -42,35 +221,160 @@ const out=(outputText : string, newline:boolean=true)=>{
     Deno.stdout.writeSync(output)
 }
 
-const main=async(url:string, outputName:string)=>{
+const showHelp=()=> {
+    const parts = Deno.mainModule.split(`/`);
+    const scriptName = parts[parts.length -1];
+
+    const gray = "\x1b[37m";
+    const bold = "\x1b[1m";
+    const italic = "\x1b[3m";
+    const reset = "\x1b[0m"; 
+
+    out(`
+        A lightweight wrapper around deno_emit. It is intended for bundling local files, npm packages, and jsr packages. For npm packages, dependencies are fetched via esm.sh. All examples here assumes that script is installed with the default name, i.e., "bundl"
+
+            ${bold}USAGE${reset}
+            ${scriptName} [Options] <filename>
+
+            ${bold}UTILITY ${reset}${gray}AND EXAMPLES${reset}
+            1. Bundle local file
+            ${gray}bundl localFile.ts -o localFile.js${reset}
+            2. Retrieve a package from jsr.io${reset}
+            ${gray}bundl -j @scope/package -o packageName.js${reset}
+            3. Retrieve a package from npm (via esm.sh and npm registry)
+            ${gray}bundl -n packageName -o packageName.js${reset}
+            esm.sh includes TypeScript types by default. To generate additional types, use the -t flag.${reset}
+            ${gray}bundl -n packageName -t -o packageName.js${reset}
+
+            ${bold}OPTIONS${reset}
+            -h   Show this help message and exit
+            -j   Retrieve package from jsr.io
+            -n   Retrieve package from npm. Uses esm.sh
+            ${gray}For generating types for npm, additionally include -t flag${reset}
+            -o   Specify the output file path or name
+
+            ${gray}${italic}link: https://github.com/tinyCodes1/bundl${reset}${reset}
+                `);
+            return;
+}
+
+const bundl = async(url:string):Promise<string>=> {
+    try {
+        const filePath = url;
+        const res = await bundle(filePath);
+        const { code } = res;
+        return code ;
+    } catch (error) {
+        if (error instanceof Error) {
+            err(`Error in ${url}`);
+        }
+    }
+    return `err`;
+}
+
+const removeExtensions = (fileName:string):string => {
+    return fileName.replace(/\.ts$/,``).replace(/\.mjs$/,``).replace(/\.js$/,``).replace(/\.b$/,``).replace(/\.d$/,``);
+}
+
+const npmDtsRetrieval = async(parts0:string, outputName:string)=>{
+    const urlNpm = `https://registry.npmjs.org/${parts0}`;
+        const metaDataNpm = readText(urlNpm);
+    if (!metaDataNpm) {
+        out(`npm metadata not found. url: ${urlNpm}`);
+        Deno.exit(0);
+    }
+    const metaJsonNpm = JSON.parse(metaDataNpm);
+    let latestVersion = ``;
+    try {
+        latestVersion = metaJsonNpm[`dist-tags`][`latest`];
+    } catch (_e) {
+        out(`npm package not found. url: ${urlNpm}`);
+        Deno.exit(0);
+    }
+    const tarUrl = metaJsonNpm[`versions`][latestVersion][`dist`][`tarball`];
+    const tempDirNpm = Deno.makeTempDirSync();
+    if (tarUrl) {
+        const tarFile = download(tarUrl, tempDirNpm+S+parts0+`.tgz`);
+        await untgz(tarFile, tempDirNpm);
+        const allDtsFiles = getFiles(tempDirNpm, true, `.d.ts$`);
+        let allDtsText = ``;
+        for (const dtsFile of allDtsFiles) {
+            const dtsText = readText(dtsFile);
+            const fileName = dtsFile.split(`/`).slice(-1).join(``);
+            allDtsText += `// ------------- ${fileName} ---------\n\n` + dtsText + `\n\n\n\n`;
+        }
+        if (allDtsText) {
+            const dtsName = removeExtensions(outputName) + `.d.ts`;
+            writeText(dtsName, allDtsText);
+            out(`Types written to: ` + dtsName);
+        }
+        removeDir(tempDirNpm);
+    }
+}
+
+const main=async(url:string, outputName:string, mode:string)=>{
     if (!url) {
         out(`Give filepath`);
         return;
     }
-    if (!outputName) {
-        outputName = url.replace(/\.js$/,`.b.js`).replace(/\.ts$/,`.b.js`).replace(/\.mjs$/,`.b.mjs`);
-        out(`Prefer giving output file name with -o flag.`);
-        const parts = url.split(`/`);
-        outputName = parts[parts.length-1].replace(/\.js$/,`.b.js`).replace(/\.ts$/,`.b.js`).replace(/\.mjs$/,`.b.mjs`);
-    }
     if (outputName === url) {
-        out(`File name error. Not overwriting. Specify output file name with -o`);
+        out(`File name error. Not overwriting. Specify another file name with -o`);
         return;
     }
     const bundledText = await bundl(url);
-    Deno.writeTextFileSync(outputName , bundledText);
-    out(`output written to: ` + outputName);
+    if (bundledText === `err`) {
+        return;
+    }
+    if (!/.js$/.test(outputName)) {
+        outputName += `.js`;
+    }
+    writeText(outputName, bundledText);
+    out(`Output written to: ` + outputName);
+    if (mode === `none`) {
+        const dtsText = getDtsText(url);
+        if (dtsText) {
+            const dtsName = removeExtensions(outputName) + `.d.ts`;
+            writeText(dtsName, dtsText);
+            out(`Types written to: ` + dtsName);
+        }
+    }
+}
+
+const getDtsText =(fileName:string, tmpJsrDir?:string)=>{
+    const tmpDtsDir = Deno.makeTempDirSync();
+    const compilerOptions: ts.CompilerOptions = {
+        declaration: true,
+        noEmit: false,
+        outDir: tmpDtsDir
+    };
+    const host = ts.createCompilerHost(compilerOptions);
+    const program = ts.createProgram([fileName], compilerOptions, host);
+    program.emit();
+    const dtsFiles: string[] = getFiles(tmpDtsDir, true, `.d.ts$`);
+    let totalText = ``;
+    for (const dtsFile of dtsFiles) {
+        const fileName = dtsFile.split(`/`).slice(-1).join(`.`).replace(`.d`,``);
+        totalText += `// ------------- ${fileName} ---------\n\n` + readText(dtsFile) + `\n\n\n\n`;
+    }
+    removeDir(tmpDtsDir);
+    if (tmpJsrDir) {
+        removeDir(tmpJsrDir);
+    }
+    return totalText;
 }
 
 // script starts here 
 type Flags = {
     h: boolean; 
+    j: boolean;
+    n: boolean;
+    t: boolean;
     o: string;
     _: string;
 };
 
 const flags:Flags = parseArgs(Deno.args, {
-    boolean: [`h`],
+    boolean: [`h`, `j`, `n`, `t`],
     string: [`o`],
     default: { h:false},
 });
@@ -78,7 +382,122 @@ const flags:Flags = parseArgs(Deno.args, {
 if (flags.h) { showHelp(); Deno.exit(0); }
 
 // setting variable for main/bundl
-const url = flags._[0];
-const outputName = flags.o;
+let url = flags._[0];
+let outputName = flags.o;
+const jsrMode = flags.j;
+const npmMode = flags.n;
+const npmTypes = flags.t;
+let mode = `none`;
 
-main(url, outputName);
+if (jsrMode) {
+    mode = `jsr`;
+    const parts = url.split(`/`);
+    if (!parts[0].startsWith(`@`)) {
+        out(`jsr path should start with @`);
+        Deno.exit(0);
+    }
+    const metaJson = readText(`https://jsr.io/${parts[0]}/${parts[1]}/meta.json`);
+        let meta = JSON.parse(`{}`);
+    try {
+        meta = JSON.parse(metaJson);
+    } catch (_err) {
+        out(`Error in retrieving jsr files. url: ${url}`);
+        Deno.exit(0);
+    }
+    const latest = meta[`latest`];
+
+    const versionJson = readText(`https://jsr.io/${parts[0]}/${parts[1]}/${latest}_meta.json`);
+        const jsonObj = JSON.parse(versionJson);
+    const exports = jsonObj[`exports`];
+    let laterParts =  parts.slice(2).join(`/`);
+    if (typeof exports !== `string`) {
+        const keyValue = JSON.parse(JSON.stringify(exports));
+        const key = `./` + laterParts;
+        if (key in keyValue) {
+            laterParts = keyValue[key];
+        }
+    }
+    if (laterParts) {
+        if (laterParts.endsWith(`.ts`)) {
+            url = `https://jsr.io/${parts[0]}/${parts[1]}/${latest}/${laterParts}`;
+                if (!outputName) {
+                out(`Prefer giving output file name with -o flag.`);
+                outputName = laterParts.replace(/\.ts$/,`.js`);
+            }
+        } else {
+            url = `https://jsr.io/${parts[0]}/${parts[1]}/${latest}/${laterParts}/mod.ts`;
+                if (!outputName) {
+                out(`Prefer giving output file name with -o flag.`);
+                outputName = laterParts + `.js`;
+            }
+        }
+    } else {
+        url = `https://jsr.io/${parts[0]}/${parts[1]}/${latest}/mod.ts`;
+            if (!outputName) {
+            out(`Prefer giving output file name with -o flag.`);
+            outputName = parts[1] + `.js`;
+        }
+    }
+    const tempDir = Deno.makeTempDirSync();
+    const mainFile = exports[`.`].replace(/^\.\//g, ``);
+        const fileList = jsonObj[`manifest`];
+    if (fileList) {
+        const filelistObj = JSON.parse(JSON.stringify(fileList));
+        for (const file in filelistObj) {
+            if ((/.ts$/).test(file)) {
+                url = `https://jsr.io/${parts[0]}/${parts[1]}/${latest}${file}`;
+                    const fileText = readText(url);
+                writeText(tempDir + S + file, fileText);
+            }
+        }
+        if (mainFile) {
+            const dtsText = getDtsText(tempDir + S + mainFile, tempDir);
+            if (dtsText) {
+                const dtsName = removeExtensions(outputName) + `.d.ts`;
+                writeText(dtsName, dtsText);
+                out(`Types written to: ` + dtsName);
+            }
+        }
+    }
+}
+
+if (npmMode) {
+    mode = `npm`;
+    const parts = url.split(`/`);
+    const urlEsm = `https://esm.sh/${parts[0]}?dev&target=deno&bundle-deps`;
+        const metaDataEsm = readText(urlEsm);
+    if (!metaDataEsm) {
+        out(`npm package not found. url: ${urlEsm}`);
+        Deno.exit(0);
+    }
+    let laterurl = ``;
+    const metaLines = metaDataEsm.split(/\n/);
+    for (const line of metaLines) {
+        const lineparts = line.split(` from `);
+        if (lineparts[0].trim() === `export *`) {
+            laterurl = lineparts[1].replace(/;/,``).replace(/['"]/g,``).trim();
+        }
+    }
+    url = `https://esm.sh${laterurl}`;
+        if (!outputName) {
+        out(`Prefer giving output file name with -o flag.`);
+        outputName = parts[0] + `.mjs`;
+    }
+    if (!outputName.match(/\.mjs$/)) {
+        outputName = removeExtensions(outputName) + `.mjs`;
+    }
+    if (npmTypes) {
+        npmDtsRetrieval(parts[0], outputName);
+    }
+}
+
+if ((!outputName && (url))) {
+    out(`Prefer giving output file name with -o flag.`);
+    const parts = url.split(`/`);
+    outputName = removeExtensions(parts[parts.length-1]) + `.js` ;
+}
+
+if (import.meta.main) {
+    await main(url, outputName, mode);
+}
+
--- readme.md
+++ readme.md
@@ -1,18 +1,54 @@
-<!-- <h2>Overview</h2> -->
-<p><code>@tinycode/bundl</code> is a simple wrapper script around
+<div style="background-color: red; color: white; padding: 5px; font-weight:bold">
+  EXPERIMENTAL
+</div>
+
+
+<br>
+<a href="https://jsr.io/@tinycode/bundl">
+<img src="https://jsr.io/badges/@tinycode/bundl" alt="JSR Badge" />
+</a>
+
+<br>
+<br>
+
+<p><span style="margin-left: 20px"><code>@tinycode/bundl</code>
+is a lightweight wrapper around 
 <a href="https://jsr.io/@deno/emit@0.46.0" target="_blank">deno_emit</a>.
-</p>
+It is intended for bundling local files, npm packages, and jsr packages. Bundling is often necessary for debugging, script minification, or creating standalone packages.
+</p><p>
+<span style="margin-left: 20px">For npm packages,
+<code>@tinycode/bundl</code>
+fetches dependencies via
+<a href="https://esm.sh" target="_blank">esm.sh</a>
+
 
 <h2>Install/Update</h2>
 <pre><code>deno install -frgA jsr:@tinycode/bundl -n bundl</code></pre>
 
 <h2>Usage</h2>
+<em>Local file</em>
 <pre><code>bundl script.ts -o output.js</code></pre>
-<p>Or</p>
-<pre><code>bundl script.js -o output.js</code></pre>
+<em>JSR package</em>
+<pre><code>bundl -j @scope/package -o packageName.js</code></pre>
+<em>NPM package</em>
+<pre><code>bundl -n packageName -o packageName.mjs</code></pre>
 
-<h2>Available Options</h2>
+
+<h2>CLI Options</h2>
 <div style="margin-left:20px;">
-<p><strong>-o</strong>  output file name</p>
 <p><strong>-h</strong>  show this help message</p>
+<p><strong>-o</strong>  output file name</p>
+<p><strong>-j</strong>  Retrieve package from jsr.io</p>
+<p><strong>-n</strong>  Retrieve npm package via esm.sh</p>
+<p><strong>-t</strong>  Retrieve types of npm packages (-n also needed)</p>
 </div>
+
+
+<h2>License</h2>
+<p><span style="margin-left: 20px">This project is licensed under the <a href="./LICENSES/LICENSE.txt">MIT License</a>.</p>
+
+<h3>Dependencies</h3>
+<p><span style="margin-left: 20px">Some dependencies may be licensed under different terms. In particular, 
+<a href="https://www.npmjs.com/package/typescript">Typescript</a> 
+is licensed under the Apache License 2.0.
+</p>
